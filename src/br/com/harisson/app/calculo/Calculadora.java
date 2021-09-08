5c285714258b2613be2526889e5f2861328b04e8
Harisson Oliveira <harisson.batista.oliveira@everis.com>
2021-09-08 14:36:57 -0300

    Adjust module-info to TRANSITIVE requires

--- src/br/com/harisson/app/calculo/Calculadora.java
+++ src/br/com/harisson/app/calculo/Calculadora.java
@@ -1,12 +1,14 @@
 package br.com.harisson.app.calculo;
 
 import br.com.harisson.app.calculo.interno.OperacoesAritmeticas;
+import br.com.harisson.app.logging.Logger;
 
 public class Calculadora {
 
 	private OperacoesAritmeticas opAritmeticas = new OperacoesAritmeticas();
 
 	public double soma(double... nums) {
+		Logger.info("Somando...");
 		return opAritmeticas.soma(nums);
 	}
 	
